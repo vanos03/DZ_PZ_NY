823d34182c9c9b6b122dd0b103020ef621413293
ivan123456 <48185734+vanos03@users.noreply.github.com>
2024-01-03 17:39:41 +0300

    Add files via upload

--- packer/loader.c
+++ packer/loader.c
@@ -1,50 +1,59 @@
+
+#include <elf.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <sys/mman.h>
 #include <sys/types.h>
-#include <fcntl.h>
+#include <sys/stat.h>
 
-typedef void (*EntryPoint)();
-
-void executeElf(const char *filename) {
+void execute_elf(char *filename) {
     int fd = open(filename, O_RDONLY);
     if (fd == -1) {
-        perror("Error opening file");
+        perror("open");
         exit(EXIT_FAILURE);
     }
 
-    // Определение размера файла
-    off_t size = lseek(fd, 0, SEEK_END);
-    lseek(fd, 0, SEEK_SET);
-
-    // Чтение содержимого файла в память
-    void *mem = mmap(NULL, size, PROT_READ | PROT_EXEC, MAP_PRIVATE, fd, 0);
-    if (mem == MAP_FAILED) {
-        perror("Error mapping file to memory");
+    struct stat file_stat;
+    if (fstat(fd, &file_stat) == -1) {
+        perror("fstat");
         close(fd);
         exit(EXIT_FAILURE);
     }
 
-    // Закрытие файла после отображения в память
+    void *elf_data = mmap(NULL, file_stat.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+    if (elf_data == MAP_FAILED) {
+        perror("mmap");
+        close(fd);
+        exit(EXIT_FAILURE);
+    }
+
+    // Определение точки входа (entry point) из ELF-заголовка
+    Elf64_Ehdr *elf_header = (Elf64_Ehdr *)elf_data;
+    Elf64_Addr entry_point = elf_header->e_entry;
+
+    // Закрытие файлового дескриптора, так как данные уже загружены в память
     close(fd);
 
-    // Выполнение ELF-файла
-    EntryPoint entryPoint = (EntryPoint)mem;
-    entryPoint();
+    // Выполнение программы, начиная с точки входа
+    void (*func)() = (void (*)())entry_point;
+    func();
 
-    // Отключение отображения в память после выполнения
-    munmap(mem, size);
+    // Отмена отображения файла из памяти
+    if (munmap(elf_data, file_stat.st_size) == -1) {
+        perror("munmap");
+        exit(EXIT_FAILURE);
+    }
 }
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
-        fprintf(stderr, "Usage: %s <elf_filename>\n", argv[0]);
+        fprintf(stderr, "Использование: %s <elf-файл>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    executeElf(argv[1]);
+    execute_elf(argv[1]);
 
     return 0;
 }
