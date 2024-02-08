22deccb2993de2f87b1645068f876be39e940d4b
ivan123456 <48185734+vanos03@users.noreply.github.com>
2024-02-08 11:39:37 -0500

    Add files via upload

--- packer/packer2/boot2.c
+++ packer/packer2/boot2.c
@@ -1,50 +1,94 @@
-#include <stdlib.h>
-#include <sys/stat.h>
+
+#include <elf.h>
 #include <stdio.h>
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <stdint.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/mman.h>
 
-#define OFFSET 16456
-#define FILENAME "elf"
+#define HEAD_SIZE 0x1000
+#define FREE(arg) free(arg);arg=NULL;
+#define MMAP_ERR(mem) if(mem==MAP_FAILED){perror("mmap");exit(EXIT_FAILURE);}
+#define MAX(x,y) (((x)>(y))?(x):(y))
 
-int main(int argc, char **argv){
-    int fd = open(argv[0], O_RDONLY);
+
+
+int execute_elf(char *filename) {
+    
+    int fd = open(filename, O_RDONLY);
     if (fd == -1) {
         perror("open");
         exit(EXIT_FAILURE);
     }
+    uint32_t elf_size = lseek(fd, 0, SEEK_END);
+    lseek(fd, 0, SEEK_SET);
+    uint8_t *elf = malloc(elf_size);
 
-    int elf_size = lseek(fd, 0, SEEK_END) - OFFSET;
-    lseek(fd, OFFSET, SEEK_SET);
-
-    uint32_t *elf_code = malloc(elf_size*sizeof(uint32_t));
-    memset(elf_code, 0, elf_size);
-
-    if (read(fd, elf_code, elf_size) <= 0){
+    if (read(fd, elf, elf_size) <= 0){
         perror("read");
         exit(EXIT_FAILURE);
     }
+    
+    Elf64_Ehdr *elf_hdr = (Elf64_Ehdr *)elf;
+    uint32_t max_vaddr = 0;
+    for(int i =0; i < elf_hdr->e_phnum; i++){
+        Elf64_Phdr *elf_seg = (Elf64_Phdr *)(elf + elf_hdr->e_phoff + (elf_hdr->e_phentsize)*i);
+        max_vaddr = MAX(max_vaddr, elf_seg->p_vaddr+elf_seg->p_memsz);
+    }
 
-    close(fd);
+    void *seg_mem = mmap(0, max_vaddr, PROT_WRITE,  MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED, -1, 0);
+    MMAP_ERR(seg_mem);
 
-    int fd_elf = open(FILENAME, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR | S_IXUSR);
-    if (fd_elf == -1) {
-        perror("open");
-        exit(EXIT_FAILURE);
-    }    
+    printf("\t----COUNT OF SEGMETS: %d----\n", elf_hdr->e_phnum);
+    for(int i =0; i < elf_hdr->e_phnum; i++){
+        printf("i = %d\n", i);
+        Elf64_Phdr *elf_seg = (Elf64_Phdr *)(elf + elf_hdr->e_phoff + (elf_hdr->e_phentsize)*i);
 
-    printf("Create \"%s\"", FILENAME);
-    if (write(fd_elf, elf_code, elf_size) <= 0){
-        perror("write");
-        close(fd_elf);
+        printf(" \
+        type: 0x%x\n \
+        число байт занимаемое сегментом в файле: 0x%x\n \
+        число байт занимаемое сегментом в памяти: 0x%x\n \
+        виртуальный адрес, по которому располагается первый байт сегмента в памяти: 0x%x\n \
+        смещение от начала файла, по которому располагается первый байт сегмента: 0x%x\n\n\n", \
+                        elf_seg->p_type, \
+                        elf_seg->p_filesz, \
+                        elf_seg->p_memsz, \
+                        elf_seg->p_vaddr, \
+                        elf_seg->p_offset
+                        );
+        // puts("---");
+        // if (elf_seg->p_type == PT_LOAD){
+        uint8_t *seg = malloc(elf_seg->p_memsz);
+            // puts("memcpy 1");
+        memcpy(seg, elf + elf_seg->p_offset, elf_seg->p_filesz);
+            // printf("elf_s = : %d\taddr + mem = : %d\n", elf_size, seg_mem + elf_seg->p_vaddr + elf_seg->p_memsz);
+            // puts("memcpy 2");
+        memcpy(seg_mem + elf_seg->p_vaddr, seg, elf_seg->p_memsz);
+            // puts("---");
+            FREE(seg);
+        // }
+    }
+    printf("\t----MPROTECT----\n");
+    mprotect(seg_mem, elf_size, PROT_READ | PROT_EXEC);
+
+    printf("\t----MUNMAP----\n");
+    if (munmap(seg_mem, elf_size) == -1) {
+        perror("munmap");
         exit(EXIT_FAILURE);
     }
-    close(fd_elf);
-    sleep(1);
-    // system ("./elf");
-    return 0;
 
+    FREE(elf);
+    close(fd);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Использование: %s <elf-файл>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    execute_elf(argv[1]);
+
+    return 0;
 }
