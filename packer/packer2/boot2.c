265afaa3e0bece63fca2de8797eea68eceaac718
ivan123456 <48185734+vanos03@users.noreply.github.com>
2024-02-08 11:35:27 -0500

    Add files via upload

--- packer/packer2/boot2.c
+++ packer/packer2/boot2.c
@@ -1,62 +1,50 @@
-
-#include <elf.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
+#include <sys/stat.h>
+#include <stdio.h>
 #include <fcntl.h>
-#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <stdint.h>
+#include <string.h>
 
-#define HEAD_SIZE 0x1000
+#define OFFSET 16456
+#define FILENAME "elf"
 
-
-int execute_elf(char *filename) {
-    
-    int fd = open(filename, O_RDONLY);
+int main(int argc, char **argv){
+    int fd = open(argv[0], O_RDONLY);
     if (fd == -1) {
         perror("open");
         exit(EXIT_FAILURE);
     }
-    uint32_t elf_size = lseek(fd, 0, SEEK_END);
-    lseek(fd, 0, SEEK_SET);
-    uint8_t *elf = malloc(elf_size);
 
-    if (read(fd, elf, elf_size) <= 0){
+    int elf_size = lseek(fd, 0, SEEK_END) - OFFSET;
+    lseek(fd, OFFSET, SEEK_SET);
+
+    uint32_t *elf_code = malloc(elf_size*sizeof(uint32_t));
+    memset(elf_code, 0, elf_size);
+
+    if (read(fd, elf_code, elf_size) <= 0){
         perror("read");
         exit(EXIT_FAILURE);
     }
-    
-    Elf64_Ehdr *elf_hdr = (Elf64_Ehdr *)elf;
-
-    printf("----COUNT OF SEGMETS: %d----\n", elf_hdr->e_phnum);
-    for(int i =0; i < elf_hdr->e_phnum; i++){
-        Elf64_Phdr *elf_seg = (Elf64_Phdr *)(elf + elf_hdr->e_phoff + (elf_hdr->e_phentsize)*i);
-
-        printf(" \
-        type: 0x%x\n \
-        число байт занимаемое сегментом в файле: 0x%x\n \
-        число байт занимаемое сегментом в памяти: 0x%x\n \
-        виртуальный адрес, по которому располагается первый байт сегмента в памяти: 0x%x\n \
-        смещение от начала файла, по которому располагается первый байт сегмента: 0x%x\n\n\n", \
-                        elf_seg->p_type, \
-                        elf_seg->p_filesz, \
-                        elf_seg->p_memsz, \
-                        elf_seg->p_vaddr, \
-                        elf_seg->p_offset
-                        );
-    }
-
 
     close(fd);
-}
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Использование: %s <elf-файл>\n", argv[0]);
+    int fd_elf = open(FILENAME, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR | S_IXUSR);
+    if (fd_elf == -1) {
+        perror("open");
+        exit(EXIT_FAILURE);
+    }    
+
+    printf("Create \"%s\"", FILENAME);
+    if (write(fd_elf, elf_code, elf_size) <= 0){
+        perror("write");
+        close(fd_elf);
         exit(EXIT_FAILURE);
     }
-
-    execute_elf(argv[1]);
-
+    close(fd_elf);
+    sleep(1);
+    // system ("./elf");
     return 0;
+
 }
